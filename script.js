b8b85de7376845facbed09342d12235664da553d
Deepak Kumar <dktiwary007@gmail.com>
2023-03-15 15:26:28 +0530

    Assignment

---
+++ script.js
@@ -0,0 +1,38 @@
+//Task-Try Arithmatic operators
+//arithmatic operators on numbers datatype.
+
+let a = 16;
+let b = 10;
+let c = a+b;
+console.log(c);
+c=a*b;
+console.log(c);
+c=a/b;
+console.log(Math.floor(c));
+c=a%b;
+console.log(c);
+c=b**6;
+console.log(c);
+
+//Arithmatic Operators with Boolean Datatype
+
+a=true;
+b=false;
+console.log(typeof(a));
+c=a+b;
+console.log(c);
+c=a-b;
+console.log(c);
+c=a*b;
+console.log(c);
+c=a/b;
+console.log(c);
+
+//Arithmatic Operators with String Datatype
+
+a="Deepak";
+b="Kumar";
+c=a+" "+b;
+console.log(c);
+c=a*3;
+console.log(c); //does not work in Javascript.
